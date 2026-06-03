2e53cd7fdad3bbed4799e3984fcd0fb5f239a7b8
haibiao_gu <13812838001@163.com>
2026-06-03 10:31:23 +0800

    fix(function): 解决本地化函数列表中缺少属性时的错误
    - 添加了对 localeFunction 存在性的检查
    - 添加了对 localeFunction.p 存在性的检查
    - 避免了访问不存在的属性导致的运行时错误
    - 确保只有当本地化属性存在时才进行对象合并
    - 提高了代码的健壮性以处理不完整的本地化数据

--- src/function/getLocalizedFunctionList.js
+++ src/function/getLocalizedFunctionList.js
@@ -3,11 +3,17 @@ import functionlist from "./functionListDescriptor";
 export default localeFunctionList => {
   functionlist.forEach(f => {
     const localeFunction = localeFunctionList[f.n];
-    f.d = localeFunction.d;
-    f.a = localeFunction.a;
-    f.p.forEach((p, i) => {
-      Object.assign(p, localeFunction.p[i]);
-    });
+    if (localeFunction) {
+      f.d = localeFunction.d;
+      f.a = localeFunction.a;
+      if (localeFunction.p) {
+        f.p.forEach((p, i) => {
+          if (localeFunction.p[i]) {
+            Object.assign(p, localeFunction.p[i]);
+          }
+        });
+      }
+    }
   });
 
   return functionlist;
